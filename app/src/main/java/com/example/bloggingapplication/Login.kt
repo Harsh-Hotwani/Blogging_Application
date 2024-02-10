01f26a3d6fad07f253f0a0ed6dc7047d8a35b35f
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2024-02-11 00:07:10 +0530

    2nd

--- app/src/main/java/com/example/bloggingapplication/Login.kt
+++ app/src/main/java/com/example/bloggingapplication/Login.kt
@@ -1,12 +1,27 @@
 package com.example.bloggingapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
+import com.example.bloggingapplication.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private val binding:ActivityLoginBinding by lazy{
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         supportActionBar!!.hide()
+
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this,Registration::class.java))
+            finish()
+        }
+
+
     }
 }
